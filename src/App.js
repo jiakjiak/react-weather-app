bfb1f6935ebd9327e17e362ac2e0ecce35de0b4e
jiakjiak <88105937+jiakjiak@users.noreply.github.com>
2022-01-15 17:19:18 -0700

    Fixed bug: video background was not running.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ export default function App() {
   return (
     <div className="App">
       <video
-        autoPlay
+        muted
+        autoPlay={"autoplay"}
+        preLoad="auto"
         loop
         style={{
           position: "fixed",
